298e312f2ec4c71ee787d9e8b6566a140cf1044b
yubo <usher2007@foxmail.com>
2012-05-27 15:51:53 +0800

    Fixed some bugs. And the result is much better.
    When assign the prob value, max the diff between different depth. Bigger than a thresh, assign the weight to 0, and other use a punishment.
    Enlarge the neighbour to 5*5 pixels.

--- Research/Kinect/SegmentKinectVideos/SegmentKinectVideos/ShowDepthVideos.cpp
+++ Research/Kinect/SegmentKinectVideos/SegmentKinectVideos/ShowDepthVideos.cpp
@@ -14,6 +14,7 @@ void saveImage(int, IplImage*, IplImage*,IplImage*,IplImage*);
 double getDepthRelationWeight(int row, int col, IplImage *pMask, double avgDepth, int index);
 void generateLikelyhood(CvMat *pProbFrMask, IplImage *pMask, IplImage *pFrame, float avgMaskDepth);
 void matMultipleNum(CvMat *pMat, double num);
+void reAdjustFrMask(IplImage *pMask, CvMat *pProbFrMask);
 double maxLikelyhood = 0.0;
 int main(int argc, char** argv)
 {
@@ -48,12 +49,12 @@ int main(int argc, char** argv)
 
 	printf("number of frames=%i\n", nofs);
 
-	cvNamedWindow("Depth Image", 1);
+	/*cvNamedWindow("Depth Image", 1);
 	cvNamedWindow("Foreground", 1);
 	cvNamedWindow("Segment Result", 1);
 	cvMoveWindow("Depth Image", 0, 0);
 	cvMoveWindow("Foreground", 0, 640);
-	cvMoveWindow("Segment Result", 480, 0);
+	cvMoveWindow("Segment Result", 480, 0);*/
 	
 	GaussBGM *bgModel= NULL;
 	int frameCount = 0;
@@ -87,10 +88,13 @@ int main(int argc, char** argv)
 			pMask->imageData = (char *)depthMap.GetForegroundMask();
 
 			pProbFrMask = cvCreateMat(scaledDepthImgSize.height, scaledDepthImgSize.width, CV_32FC1);
-			generateLikelyhood(pProbFrMask, pMask, pFrame, depthMap.GetAvgMaskValue()/16);
+			generateLikelyhood(pProbFrMask, pMask, pFrame, depthMap.GetAvgMaskValue());
 //ForDebug
-matMultipleNum(pProbFrMask, 1.0 / maxLikelyhood);	
-			cvGetImage(pProbFrMask, pProbForShow);
+//matMultipleNum(pProbFrMask, 255.0 / maxLikelyhood);	
+//cvGetImage(pProbFrMask, pProbForShow);
+
+			reAdjustFrMask(pMask, pProbFrMask);
+
 
 			memset(pFrResImg->imageData, 0, pFrame->imageSize);
 			cvCopy(pFrame, pFrResImg, pMask);
@@ -98,10 +102,11 @@ matMultipleNum(pProbFrMask, 1.0 / maxLikelyhood);
 			pBkImg = bgModel->GetBackgroundImg();
 			pFrImg = bgModel->GetForegroundImg();
 
-			cvShowImage("Depth Image", pProbForShow);
+			/*cvShowImage("Depth Image", pProbForShow);
 			cvShowImage("Foreground", pMask);
-			cvShowImage("Segment Result", pFrResImg);
-			//saveImage(frameCount, pFrame, pScaledDepthImage, pFrImg, pFrResImg);
+			cvShowImage("Segment Result", pFrResImg);*/
+			saveImage(frameCount, pFrame, pMask, pFrImg, pFrResImg);
+			std::cout<<"frame cout:"<<frameCount<<std::endl;
 
 			cvWaitKey(2);
 			cvReleaseImage(&pScaledDepthImage);
@@ -125,7 +130,7 @@ void generateLikelyhood(CvMat *pProbFrMask, IplImage *pMask, IplImage *pFrame, f
 	{
 		for(int j=0; j<pMask->width; j++)
 		{
-			if(i==0 || i==pMask->height-1 || j==0 || j==pMask->width-1)
+			if(i<=5 || i>=pMask->height-6 || j<=5 || j>=pMask->width-6)
 			{
 				cvmSet(pProbFrMask, i, j, 0.0);
 				continue;
@@ -156,16 +161,16 @@ double getColorRelationWeight(int row, int col, IplImage *pFrame, int index)
 	switch(index)
 	{
 	case 0:
-		sSide = cvGet2D(pFrame, row, col-1);
+		sSide = cvGet2D(pFrame, row, col-6);
 		break;
 	case 1:
-		sSide = cvGet2D(pFrame, row-1, col);
+		sSide = cvGet2D(pFrame, row-6, col);
 		break;
 	case 2:
-		sSide = cvGet2D(pFrame, row, col+1);
+		sSide = cvGet2D(pFrame, row, col+6);
 		break;
 	case 3:
-		sSide = cvGet2D(pFrame, row+1, col);
+		sSide = cvGet2D(pFrame, row+6, col);
 		break;
 	default:
 		return 0.0;
@@ -188,22 +193,22 @@ double getDepthRelationWeight(int row, int col, IplImage *pMask, double avgDepth
 	switch(index)
 	{
 	case 0:
-		neighbourDepth = maskDepthData[row*pMask->widthStep + col-1];
+		neighbourDepth = maskDepthData[row*pMask->widthStep + col-6];
 		break;
 	case 1:
-		neighbourDepth = maskDepthData[(row-1)*pMask->widthStep + col];
+		neighbourDepth = maskDepthData[(row-6)*pMask->widthStep + col];
 		break;
 	case 2:
-		neighbourDepth = maskDepthData[row*pMask->widthStep + col+1];
+		neighbourDepth = maskDepthData[row*pMask->widthStep + col+6];
 		break;
 	case 3:
-		neighbourDepth = maskDepthData[(row+1)*pMask->widthStep + col];
+		neighbourDepth = maskDepthData[(row+6)*pMask->widthStep + col];
 		break;
 	default:
 		return 0.0;
 		break;
 	}
-	if(neighbourDepth == 0)
+	if(neighbourDepth == 0 || neighbourDepth > avgDepth * 1.2)
 		return 0.0;
 
 	return avgDepth/neighbourDepth;
@@ -219,6 +224,25 @@ void matMultipleNum(CvMat *pMat, double num)
 			cvmSet(pMat, i, j, val);
 		}
 	return;
+}
+
+void reAdjustFrMask(IplImage *pMask, CvMat *pProbFrMask)
+{
+	uchar *maskData = (uchar *)pMask->imageData;
+	for(int i=0; i<pProbFrMask->rows; i++)
+	{
+		for(int j=0; j<pProbFrMask->cols; j++)
+		{
+			if(cvmGet(pProbFrMask, i, j) > maxLikelyhood*0.7)
+			{
+				maskData[i*pMask->widthStep+j] = 255;
+			}
+			else
+			{
+				maskData[i*pMask->widthStep+j] = 0;
+			}
+		}
+	}
 }
 
 string resultFolder = "D:\\Navigation\\Research\\Kinect\\SegmentKinectVideos\\Data\\";
