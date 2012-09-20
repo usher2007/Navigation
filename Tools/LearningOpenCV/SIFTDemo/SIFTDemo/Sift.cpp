0fb9ecb96a7b7c3e494aadbceb7a2cb4f6be20ed
Yubo.Guo <yubo@topmoo.cn>
2012-09-20 16:01:33 +0800

    Draw the key points and save the image.

--- Tools/LearningOpenCV/SIFTDemo/SIFTDemo/Sift.cpp
+++ Tools/LearningOpenCV/SIFTDemo/SIFTDemo/Sift.cpp
@@ -13,10 +13,10 @@ using namespace cv;
 int main(int argc, char** argv)
 {
 	char fileName[1024];
-
+	namedWindow("SIFT Demo", 0);
 	if(argc == 1)
 	{
-		sprintf(fileName, "H:\\Example.jpg");
+		sprintf(fileName, "H:\\Example1.jpg");
 	}
 	else
 	{
@@ -27,4 +27,16 @@ int main(int argc, char** argv)
 	vector<KeyPoint> keypoints;
 	Ptr<FeatureDetector> detector = FeatureDetector::create("SIFT");
 	detector->detect(img, keypoints);
+	vector<KeyPoint>::iterator kpIter = keypoints.begin();
+	int i = 0;
+	for(;kpIter != keypoints.end(); kpIter ++)
+	{
+		circle(img, kpIter->pt, 2, Scalar(0, 0, 255));
+		i ++;
+		if(kpIter->size < 100)
+			break;
+	}
+	imshow("SIFT Demo", img);
+	waitKey(0);
+	imwrite("H:\\Example1.bmp", img);
 }
