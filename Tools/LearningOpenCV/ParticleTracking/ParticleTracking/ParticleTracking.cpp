d7367467d5d1c4888506c2a15626db71d40f8df2
Yubo.Guo <yubo@topmoo.cn>
2012-10-08 16:48:38 +0800

    Keep developing the particle tracking. Not completed.

--- Tools/LearningOpenCV/ParticleTracking/ParticleTracking/ParticleTracking.cpp
+++ Tools/LearningOpenCV/ParticleTracking/ParticleTracking/ParticleTracking.cpp
@@ -1,4 +1,5 @@
 #include "ParticleTracking.h"
+#include <time.h>
 using namespace cv;
 
 Mat image;
@@ -6,7 +7,8 @@ int trackObject = 0;
 bool selectObject = false;
 Rect selection;
 Point origin;
-
+int histSize = 16;
+int CalcHsvHist(Mat &hist, Mat image, Rect &selectRoi, const float *phranges);
 void onMouse( int event, int x, int y, int, void* )
 {
 	if( selectObject )
@@ -40,20 +42,40 @@ int main(int argc, char **argv)
 	cap.open(videoFileName);
 
 	ParticleTrackingAlg trackingAlg;
-	Mat frame;
+	Mat frame, hsv, hue, mask;
+	float hranges[] = {0,180};
+	const float* phranges = hranges;
 	cap >> frame;
 	frame.copyTo(image);
 	namedWindow( "Particle Demo", 0 );
-	imshow("Particle Demo", frame);
 	setMouseCallback( "Particle Demo", onMouse, 0 );
+	imshow("Particle Demo", image);
 	for(;;)
 	{
 		if(trackObject < 0)
 		{
-			
+			trackingAlg.originParticle.xPos = selection.x;
+			trackingAlg.originParticle.yPos = selection.y;
+			trackingAlg.originParticle.width = selection.width;
+			trackingAlg.originParticle.height = selection.height;
+			trackingAlg.originParticle.weight = 0.0;
 			break;
 		}
+		if( selectObject && selection.width > 0 && selection.height > 0 )
+		{
+			Mat roi(image, selection); 
+			bitwise_not(roi, roi);
+		}
+		imshow("Particle Demo", image);
+		waitKey(10);
 	}
+	Rect lastObjectRegion;
+	frame.copyTo(image);
+	lastObjectRegion.x = trackingAlg.originParticle.xPos;
+	lastObjectRegion.y = trackingAlg.originParticle.yPos;
+	lastObjectRegion.width = trackingAlg.originParticle.width;
+	lastObjectRegion.height = trackingAlg.originParticle.height;
+	CalcHsvHist(trackingAlg.originHist, image, lastObjectRegion, phranges);
 	for(;;)
 	{
 		cap >> frame;
@@ -61,6 +83,125 @@ int main(int argc, char **argv)
 		{
 			break;
 		}
-
+		frame.copyTo(image);
+		lastObjectRegion.x = trackingAlg.originParticle.xPos;
+		lastObjectRegion.y = trackingAlg.originParticle.yPos;
+		lastObjectRegion.width = trackingAlg.originParticle.width;
+		lastObjectRegion.height = trackingAlg.originParticle.height;
+		CalcHsvHist(trackingAlg.originHist, image, lastObjectRegion, phranges);
 	}
+}
+
+int CalcHsvHist(Mat &hist, Mat image, Rect &selectRoi, const float *phranges)
+{
+	Mat hsv,  hue, mask;
+	cvtColor(image, hsv, CV_BGR2HSV);
+	inRange(hsv, Scalar(0, 0, 0),
+		Scalar(180, 256, 256), mask);
+	hue.create(hsv.size(), hsv.depth());
+	int ch[] = {0, 0};
+	mixChannels(&hsv, 1, &hue, 1, ch, 1);
+
+	Mat roi(hue, selectRoi), maskroi(mask, selectRoi);
+	calcHist(&roi, 1, 0, maskroi,hist, 1, &histSize, &phranges);
+	normalize(hist, hist, 0, 255, CV_MINMAX);
+	return 0;
+}
+
+double ParticleTrackingAlg::calcDistance(const Mat &histOrig, const Mat &histTmp)
+{
+	double distanceSum = 0.0, firstLen = 0.0, secondLen = 0.0;
+	for(int i=0; i<histSize; i++)
+	{
+		firstLen += (histOrig.data[i] * histOrig.data[i]);
+		secondLen += (histTmp.data[i] * histTmp.data[i]);
+		double dist = histOrig.data[i] - histTmp.data[i];
+		distanceSum += (dist * dist);
+	}
+	return sqrt(distanceSum)/(sqrt(firstLen * secondLen));
+}
+
+int ParticleGroup::normalize()
+{
+	std::vector<Particle>::iterator particleIter; 
+	double weightSum = 0.0;
+	for(particleIter = ParticleList.begin(); particleIter != ParticleList.end(); ++particleIter)
+	{
+		weightSum += particleIter->weight;
+	}
+
+	for(particleIter = ParticleList.begin(); particleIter != ParticleList.end(); ++particleIter)
+	{
+		particleIter->weight /= weightSum;
+	}
+	return 0;
+}
+
+double getRand(double min, double max)
+{
+	int randRaw = rand()%100 + 1;
+	double result = (randRaw - 1)*1.0/(100 - 1)*(max - min) + min;
+	return result;
+}
+
+int ParticleGroup::resampleParticle()
+{
+	int particleNum = ParticleList.size();
+	double *cumPdf = new double[particleNum];
+	int *newParticleIndex = new int [particleNum];
+	std::vector<Particle>::iterator particleIter;
+	std::vector<Particle> newParticleList;
+	double sum = 0.0;
+	int index = 0;
+	for(particleIter = ParticleList.begin(); particleIter != ParticleList.end(); ++particleIter)
+	{
+		sum += particleIter->weight;
+		cumPdf[index] = sum;
+		index++;
+	}
+
+	double randNum = 0.0;
+	srand(time(0));
+	particleIter = ParticleList.begin();
+	RNG rng;
+	for(int i=0; i<particleNum; i++)
+	{
+		randNum = getRand(0.0, sum);
+		int j;
+		for(j=0; j<particleNum; j++)
+		{
+			if(cumPdf[j] < randNum)
+			{
+				continue;
+			}
+			else
+			{
+				break;
+			}
+		}
+		newParticleIndex[i] = j;
+		Particle p((particleIter+j)->xPos, (particleIter+j)->yPos, (particleIter+j)->width, (particleIter+j)->height);
+		p.weight = (particleIter+j)->weight;
+		p.xPos = p.xPos + rng.gaussian(xNoise);
+		p.yPos = p.yPos + rng.gaussian(yNoise);
+		newParticleList.push_back(p);
+	}
+
+	ParticleList = newParticleList;
+	return 0;
+}
+
+int ParticleGroup::generateParticleList(const Particle &originParticle, int num)
+{
+	RNG rng;
+	for(int i=0; i<num; i++)
+	{
+		Particle p(originParticle.xPos, originParticle.yPos, originParticle.width, originParticle.height);
+		p.weight = originParticle.weight;
+		p.xPos = p.xPos + rng.gaussian(xNoise);
+		p.yPos = p.yPos + rng.gaussian(yNoise);
+		ParticleList.push_back(p);
+	}
+
+	return 0;
 }
--- Tools/LearningOpenCV/ParticleTracking/ParticleTracking/ParticleTracking.h
+++ Tools/LearningOpenCV/ParticleTracking/ParticleTracking/ParticleTracking.h
@@ -2,8 +2,9 @@
 #include "opencv2/video/tracking.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include <iostream>
 using namespace cv;
-const string videoFileName = "H:\\GitHubCode\\Navigation\\Research\\ParticleFiltering\FromShunli\Video\MVI_2132.avi";
+const string videoFileName = "H:\\GitHubCode\\Navigation\\Research\\ParticleFiltering\\FromShunli\\Video\\MVI_2132.avi";
 const int offsetFrame = 10;
 const int particleNum = 10;
 const int histBinNum = 6;
@@ -14,25 +15,35 @@ const int yNoise = 2;
 const int widthNoise = 0;
 const int heightNoise = 0;
 const double noiseWeight = 0.05;
-class Particle;
-class ParticleTrackingAlg
-{
-public:
-	int ProcessNextFrame(Mat &img);
-protected:
-	int calcParticleFeature(Mat &img, Particle p);
-	int resampleParticle();
-	double calcDistance(const Mat &histOrig, const Mat &histTmp);
-private:
-	Mat hist, originHist;
-	Mat roi;
-	std::vector<Particle> particleList;
-	std::vector<double> particlePdf;
-};
-
 class Particle
 {
 public:
-	Particle(int x, int y, int w, int h):xPos(x), yPos(y), width(w), height(h){}
+	Particle(){xPos = yPos = width = height = 0; weight = 0.0;}
+	Particle(int x, int y, int w, int h):xPos(x), yPos(y), width(w), height(h){ weight = 0.0; }
 	int xPos, yPos, width, height;
+	double weight;
 };
+
+class ParticleGroup
+{
+public:
+	std::vector<Particle> ParticleList;
+	int resampleParticle();
+	int normalize();
+	int generateParticleList(const Particle &originParticle, int num);
+};
+class ParticleTrackingAlg
+{
+public:
+	ParticleTrackingAlg(){}
+	int ProcessNextFrame(Mat &img);
+	int calcParticleFeature(Mat &img, Particle p);
+	int resampleParticle();
+	double calcDistance(const Mat &histOrig, const Mat &histTmp);
+	Mat hist, originHist;
+	Mat roi;
+	ParticleGroup particleGroup;
+	Particle originParticle;
+	std::vector<double> particlePdf;
+};
+
