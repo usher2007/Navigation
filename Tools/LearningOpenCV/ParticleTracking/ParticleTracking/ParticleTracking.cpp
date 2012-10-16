6dae1193c089234885da1768152cc94becd875cb
Yubo.Guo <yubo@topmoo.cn>
2012-10-16 14:13:05 +0800

    Use Bhattacharyya coefficient to calc distance.

--- Tools/LearningOpenCV/ParticleTracking/ParticleTracking/ParticleTracking.cpp
+++ Tools/LearningOpenCV/ParticleTracking/ParticleTracking/ParticleTracking.cpp
@@ -9,7 +9,7 @@ Rect selection;
 Point origin;
 int hbins = 6, sbins = 6, vbins = 6;
 int histSize[] = {hbins, sbins, vbins};
-float hranges[] = {0, 180};
+float hranges[] = {0, 256};
 float sranges[] = {0, 256};
 float vranges[] = {0, 256};
 const float* phranges[] = {hranges, sranges, vranges};
@@ -88,6 +88,8 @@ int main(int argc, char **argv)
 	lastObjectRegion &= Rect(0, 0, image.cols, image.rows);
 	CalcHsvHist(trackingAlg.originHist, image, lastObjectRegion);
 	int frameCount = 0;
+	double dist = 0.0;
+	double pdf = 0.0;
 	for(;;)
 	{
 		cap >> frame;
@@ -108,8 +110,8 @@ int main(int argc, char **argv)
 			particleRegion.height = particleIter->height;
 			particleRegion &= Rect(0, 0, image.cols, image.rows);
 			CalcHsvHist(trackingAlg.hist, image, particleRegion);
-			double dist = trackingAlg.calcDistance(trackingAlg.originHist, trackingAlg.hist);
-			double pdf = 1/(sqrt(2*PI)*noiseWeight)*exp(-(1-dist)/2/(noiseWeight*noiseWeight));
+			dist = trackingAlg.calcDistance(trackingAlg.originHist, trackingAlg.hist);
+			pdf = 1/(sqrt(2*PI)*noiseWeight)*exp(-(1-dist)/2/(noiseWeight*noiseWeight));
 			sumpdf += pdf;
 			particleWeight[index] = pdf;
 			index++;
@@ -135,7 +137,7 @@ int main(int argc, char **argv)
 		trackingAlg.originParticle.yPos = newY;
 		lastObjectRegion.x = trackingAlg.originParticle.xPos;
 		lastObjectRegion.y = trackingAlg.originParticle.yPos;
-		std::cout<<"Frame Count: "<<frameCount<<"\n"<<"X:"<<newX<<" Y:"<<newY<<std::endl;
+		//std::cout<<"Frame Count: "<<frameCount<<"\n"<<"X:"<<newX<<" Y:"<<newY<<std::endl;
 		frameCount ++;
 		Point pt1, pt2;
 		pt1.x = newX;
@@ -151,7 +153,7 @@ int main(int argc, char **argv)
 		int ch[] = {0, 0};
 		mixChannels(&hsv, 1, &hue, 1, ch, 1);
 		imshow("Particle Demo", image);
-		imshow("Debug", hue);
+		//imshow("Debug", hue);
 		waitKey(2);
 	}
 }
@@ -161,7 +163,7 @@ int CalcHsvHist(Mat &hist, Mat image, Rect &selectRoi)
 	Mat hsv,  hue, mask;
 	cvtColor(image, hsv, CV_BGR2HSV);
 	inRange(hsv, Scalar(0, 30, 10),
-		Scalar(180, 255, 255), mask);
+		Scalar(256, 256, 256), mask);
 	hue.create(hsv.size(), hsv.depth());
 	int ch[] = {0, 0};
 	mixChannels(&hsv, 1, &hue, 1, ch, 1);
@@ -169,14 +171,8 @@ int CalcHsvHist(Mat &hist, Mat image, Rect &selectRoi)
 	Mat roi(hsv, selectRoi), maskroi(mask, selectRoi);
 	int channels[3] = {0, 1, 2};
 	calcHist(&roi, 1, channels, Mat(), hist, 3, histSize, phranges);
-	int num = 0;
-	for(int i=0; i<6; i++)
-		for(int j=0; j<6; j++)
-			for(int k=0; k<6; k++)
-			{
-				num = hist.at<float>(i,j,k);
-			}
 	Scalar histSum = sum(hist);
+
 	hist = hist / histSum.val[0];
 	return 0;
 }
@@ -185,7 +181,9 @@ double ParticleTrackingAlg::calcDistance(const Mat &histOrig, const Mat &histTmp
 {
 	double distanceSum = 0.0, firstLen = 0.0, secondLen = 0.0;
 	//std::cout<<"========="<<std::endl<<histOrig<<std::endl<<histTmp<<std::endl;
-	distanceSum = histOrig.dot(histTmp);
+	Mat result = histOrig.mul(histTmp);
+	sqrt(result, result);
+	distanceSum = sum(result).val[0];
 	return distanceSum;
 }
 
