1cd90223bed33ca601cdc0ccd3b6928d438197e2
Yubo.Guo <yubo@topmoo.cn>
2012-11-08 11:23:26 +0800

    Test Code of Gaussian Background Model.
    The performance in release mode is soso.
    Could use BackgroundSubtractorMOG2 to try more parameters.

---
+++ Tools/LearningOpenCV/GaussianBGM/GaussianBGM/GaussianBGM.cpp
@@ -0,0 +1,30 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/video/background_segm.hpp"
+using namespace cv;
+
+const string videoFileName = "G:\\Users\\Usher\\Desktop\\学生起立检测数据\\MVI_3395_Compress.AVI";
+int main(void)
+{
+	VideoCapture cap;
+	bool res = cap.open(videoFileName);
+	Mat frame, foreground;
+	BackgroundSubtractorMOG mog;
+	namedWindow("Original");
+	namedWindow("Results1");
+	namedWindow("Results2");
+	for(;;)
+	{
+		cap >> frame;
+		if(frame.empty())
+		{
+			break;
+		}
+		mog(frame, foreground, 0.01);
+		imshow("Original", frame);
+		imshow("Result1", foreground);
+		threshold(foreground, foreground, 128, 255, THRESH_BINARY_INV);
+		imshow("Result2", foreground);
+		waitKey(10);
+	}
+}
