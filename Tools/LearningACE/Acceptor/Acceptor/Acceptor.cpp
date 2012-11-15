76b197692b8e59c13bda4e262b2ad585bc0cadee
Yubo.Guo <yubo@topmoo.cn>
2012-11-15 16:44:34 +0800

    Add read event handler to both acceptor and connector.

--- Tools/LearningACE/Acceptor/Acceptor/Acceptor.cpp
+++ Tools/LearningACE/Acceptor/Acceptor/Acceptor.cpp
@@ -5,14 +5,31 @@
 #include "ace/SOCK_Acceptor.h"
 #include <iostream>
 
+#define  DATA_SIZE 12
 class My_Svc_Handler: public ACE_Svc_Handler <ACE_SOCK_STREAM,ACE_NULL_SYNCH>
 {
 public:
+	My_Svc_Handler()
+	{
+		data = new char[DATA_SIZE];
+	}
+	
 	int open(void *)
 	{
 		std::cout<<"Connection established"<<std::endl;
+
+		ACE_Reactor::instance()->register_handler(this, ACE_Event_Handler::READ_MASK);
 		return 0;
 	}
+
+	int handle_input(ACE_HANDLE)
+	{
+		peer().recv_n(data, DATA_SIZE);
+		ACE_OS::printf("<< %s\n", data);
+		return 0;
+	}
+private:
+	char *data;
 };
 
 typedef ACE_Acceptor<My_Svc_Handler, ACE_SOCK_ACCEPTOR> MyAcceptor;
--- Tools/LearningACE/Acceptor/Connector/Connector.cpp
+++ Tools/LearningACE/Acceptor/Connector/Connector.cpp
@@ -5,14 +5,33 @@
 #include "ace/SOCK_Connector.h"
 #include <iostream>
 
+#define  DATA_SIZE 12
 class My_Svc_Handler: public ACE_Svc_Handler <ACE_SOCK_STREAM,ACE_NULL_SYNCH>
 {
 public:
+	My_Svc_Handler()
+	{
+		data = new char[DATA_SIZE];
+	}
+
 	int open(void *)
 	{
 		std::cout<<"Connection established"<<std::endl;
+
+		ACE_Reactor::instance()->register_handler(this, ACE_Event_Handler::WRITE_MASK);
 		return 0;
 	}
+
+	int handle_output(ACE_HANDLE)
+	{
+		strcpy(data, "12345678901");
+		peer().send_n(data, 12);
+		peer().send_n(data, 12);
+		ACE_OS::printf("Send: %s\n", data);
+		return 0;
+	}
+private:
+	char *data;
 };
 
 typedef ACE_Connector<My_Svc_Handler, ACE_SOCK_CONNECTOR> MyConnector;
