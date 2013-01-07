ca5025c25abc60e14189535003b1b280177f07f4
Yubo(XP-I7) <usher2007@foxmail.com>
2013-01-07 13:59:32 +0800

    SmartClassroom. The main GUI project.

---
+++ App/SmartClassroom/SmartClassroom/SmartClassroomDlg.h
@@ -0,0 +1,31 @@
+
+// SmartClassroomDlg.h : header file
+//
+
+#pragma once
+
+
+// CSmartClassroomDlg dialog
+class CSmartClassroomDlg : public CDialogEx
+{
+// Construction
+public:
+	CSmartClassroomDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_SMARTCLASSROOM_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
