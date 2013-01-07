61cd5c930504b4606d56f05b92a8c0707d40bdd0
Yubo(XP-I7) <usher2007@foxmail.com>
2013-01-07 13:52:44 +0800

    SmartClassroom. Position Analyzer Module.

---
+++ App/SmartClassroom/Analyzer/Analyzer.h
@@ -0,0 +1,19 @@
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the ANALYZER_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// ANALYZER_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef ANALYZER_EXPORTS
+#define ANALYZER_API __declspec(dllexport)
+#else
+#define ANALYZER_API __declspec(dllimport)
+#endif
+
+// This class is exported from the Analyzer.dll
+class ANALYZER_API CPositionAnalyzer {
+public:
+	CPositionAnalyzer(void);
+	// TODO: add your methods here.
+};
+
