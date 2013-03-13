fb78db9143cb56f99d4b5212a35841265aa26885
Yubo <usher.guo@outlook.com>
2013-03-13 09:42:45 +0800

    Resample the particles using the cumsum weight. Not good.

--- Research/MyTracking/main.m
+++ Research/MyTracking/main.m
@@ -3,8 +3,8 @@ fprefix = 'data\car4\';
 fannote = 'car4.txt';
 fext = 'bmp';
 s_annotation = strcat(fprefix, fannote);
-start_frame = 1;
-nframes = 659;
+start_frame = 20;
+nframes = 600;
 
 n_row = 3;
 n_col = 3;
@@ -19,14 +19,14 @@ hog_bin_size = 5;
 hog_n_orient = 9;
 hog_index = 1;
 
-particle_num = 200;
+particle_num = 800;
 particles = zeros(particle_num, 4);
-sigma_px = 2;
-sigma_py = 2;
-sigma_pw = 1;
-sigma_ph = 1;
+sigma_px = 5;
+sigma_py = 3;
+sigma_pw = 2;
+sigma_ph = 2;
 
-param.lambda=0.5;
+param.lambda=1;
 param.numThreads=-1; % number of threads
 
 param.mode=0;
@@ -34,6 +34,7 @@ param.approx=0;
 param.pos = true;
 
 template_num = 10;
+sim_thresh = 3;
 s_frames = cell(nframes, 1);
 for i=1:nframes
     image_no = start_frame + i - 1;
@@ -43,7 +44,7 @@ end
 
 %for the 1st frame, get the true object
 truth = load(s_annotation);
-first_loc = truth(1,:);
+first_loc = truth(start_frame,:);
 image_gray = imread(s_frames{1});
 if(size(image_gray,3) ~= 1)
     image_gray = rgb2gray(image_gray);
@@ -61,24 +62,27 @@ object_patches = im_get_patches(object, n_row, n_col);
 hog_row = ceil(t_row/n_row)/hog_bin_size;
 hog_col = ceil(t_col/n_col)/hog_bin_size;
 hog_feature_len = hog_row*hog_col*hog_n_orient;
-HT = zeros(n_row*n_col, 1+template_num+hog_feature_len, hog_feature_len);
+HT = zeros(n_row*n_col, 1+template_num+hog_feature_len*2, hog_feature_len);
 for i=1:n_row*n_col
     HT(i, 1, :) = my_hog(object_patches(i, :, :), hog_bin_size, hog_n_orient, hog_index);
 end
 % trival templates
 for i=1:n_row*n_col
-    HT(i, 12:end, :) = eye(hog_feature_len);
+    HT(i, 2+template_num:1+template_num+hog_feature_len, :) = eye(hog_feature_len);
+    HT(i, 2+template_num+hog_feature_len:end,:) = -1*eye(hog_feature_len);
 end
 
 State = [col_begin, row_begin, col_width, row_height];
-particles(:,1) = State(1) + fix(sigma_px*randn(1,particle_num));
-particles(:,2) = State(2) + fix(sigma_py*randn(1,particle_num));
-particles(:,3) = State(3) + fix(sigma_pw*randn(1,particle_num));
-particles(:,4) = State(4) + fix(sigma_ph*randn(1,particle_num));
+particles(:,1) = State(1) + round(sigma_px*randn(1,particle_num));
+particles(:,2) = State(2) + round(sigma_py*randn(1,particle_num));
+particles(:,3) = State(3) + round(sigma_pw*randn(1,particle_num));
+particles(:,4) = State(4) + round(sigma_ph*randn(1,particle_num));
 
 particle_features = zeros(n_row*n_col, particle_num, hog_feature_len);
-particle_alpha = zeros(n_row*n_col, 1+template_num+hog_feature_len, particle_num);
-particle_sim = zeros(n_row*n_col, particle_num);
+particle_alpha = zeros(n_row*n_col, 1+template_num+hog_feature_len*2, particle_num);
+patch_particle_sim = zeros(n_row*n_col, particle_num);
+particle_sim = zeros(1,particle_num);
+particle_reind = zeros(1,particle_num);
 for i=2:nframes
     image_gray = imread(s_frames{i});
     if(size(image_gray,3) ~= 1)
@@ -100,8 +104,33 @@ for i=2:nframes
     
     for j=1:n_row*n_col
         patch_particle_feas = reshape(particle_features(j,:,:), particle_num, hog_feature_len);
-        patch_HT = reshape(HT(j,:,:), 1+template_num+hog_feature_len, hog_feature_len);
+        patch_HT = reshape(HT(j,:,:), 1+template_num+hog_feature_len*2, hog_feature_len);
         particle_alpha(j,:,:) = my_lasso(patch_particle_feas', patch_HT', param);
-        particle_sim(j,:) = reshape((sum(particle_alpha(j,1:1+template_num,:))+0.01)./(sum(particle_alpha(j,2+template_num:end,:))+0.01), 1, particle_num);
-    end   
+        patch_particle_sim(j,:) = reshape((sum(particle_alpha(j,1:1+template_num,:))+0.01)./(sum(particle_alpha(j,2+template_num:end,:))+0.01), 1, particle_num);
+    end
+    big_sim_index = patch_particle_sim > sim_thresh;
+    particle_sim = sum(big_sim_index.*patch_particle_sim);
+    particle_sim = particle_sim / sum(particle_sim);
+    best_index = find(particle_sim == max(particle_sim));
+    State = round(mean(particles(best_index, :), 1));
+    State
+    %max(particle_sim)
+    % Generate the new particles
+    cdf_sim = cumsum(particle_sim);
+    for j=1:particle_num
+        index = find(cdf_sim>=rand);
+        particle_reind(1,j) = index(1);
+    end
+    particles = particles(particle_reind, :);
+    particles(:,1) =  particles(:,1) + round(sigma_px*randn(particle_num,1));
+    particles(:,2) =  particles(:,2) + round(sigma_py*randn(particle_num,1));
+    particles(:,3) =  particles(:,3) + round(sigma_pw*randn(particle_num,1));
+    particles(:,4) =  particles(:,4) + round(sigma_ph*randn(particle_num,1));
+    
+    % Update the template
+    index = mod(i-1, template_num);
+    if index == 0
+        index = 10;
+    end
+    HT(:,index+1,:) = mean(particle_features(:, best_index, :), 2);
 end
