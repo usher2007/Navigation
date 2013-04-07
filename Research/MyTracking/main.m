6622e869d0e2aa98f631d9cc3ae167c3eec4407e
Yubo <usher.guo@outlook.com>
2013-04-07 08:50:14 +0800

    Add gray feature. result is bad.

--- Research/MyTracking/main.m
+++ Research/MyTracking/main.m
@@ -21,10 +21,10 @@ hog_index = 1;
 
 particle_num = 800;
 particles = zeros(particle_num, 4);
-sigma_px = 5;
+sigma_px = 3;
 sigma_py = 3;
-sigma_pw = 2;
-sigma_ph = 2;
+sigma_pw = 1;
+sigma_ph = 1;
 
 param.lambda=1;
 param.numThreads=-1; % number of threads
@@ -35,7 +35,8 @@ param.pos = true;
 
 template_num = 10;
 template_weight = zeros(1,10);
-sim_thresh = 0;
+sim_thresh_hog = 0.0005;
+sim_thresh_gray = 0.0005;
 s_frames = cell(nframes, 1);
 template_decay = 0.01;
 for i=1:nframes
@@ -65,13 +66,25 @@ hog_row = ceil(t_row/n_row)/hog_bin_size;
 hog_col = ceil(t_col/n_col)/hog_bin_size;
 hog_feature_len = hog_row*hog_col*hog_n_orient;
 HT = zeros(n_row*n_col, 1+template_num+hog_feature_len*2, hog_feature_len);
+
+gray_feature_h = 15;
+gray_feature_w = 12;
+gray_feature_len = gray_feature_h * gray_feature_w;
+GT = zeros(n_row*n_col, 1+template_num+gray_feature_len*2, gray_feature_len);
+[n_patch patch_h patch_w] = size(object_patches);
+hog_weight = 0.5;
+gray_weight = 0.5;
 for i=1:n_row*n_col
     HT(i, 1, :) = my_hog(object_patches(i, :, :), hog_bin_size, hog_n_orient, hog_index);
+    object_patch_resize = imresize(reshape(object_patches(i,:,:), patch_h, patch_w), [gray_feature_h gray_feature_w]);
+    GT(i, 1, :) = reshape(object_patch_resize, gray_feature_len, 1);
 end
 % trival templates
 for i=1:n_row*n_col
     HT(i, 2+template_num:1+template_num+hog_feature_len, :) = eye(hog_feature_len);
     HT(i, 2+template_num+hog_feature_len:end,:) = -1*eye(hog_feature_len);
+    GT(i, 2+template_num:1+template_num+gray_feature_len, :) = eye(gray_feature_len);
+    GT(i, 2+template_num+gray_feature_len:end,:) = -1*eye(gray_feature_len);
 end
 
 State = [col_begin, row_begin, col_width, row_height];
@@ -80,10 +93,15 @@ particles(:,2) = State(2) + round(sigma_py*randn(1,particle_num));
 particles(:,3) = State(3) + round(sigma_pw*randn(1,particle_num));
 particles(:,4) = State(4) + round(sigma_ph*randn(1,particle_num));
 
-particle_features = zeros(n_row*n_col, particle_num, hog_feature_len);
-particle_alpha = zeros(n_row*n_col, 1+template_num+hog_feature_len*2, particle_num);
-patch_particle_sim = zeros(n_row*n_col, particle_num);
-particle_sim = zeros(1,particle_num);
+particle_features_hog = zeros(n_row*n_col, particle_num, hog_feature_len);
+particle_features_gray = zeros(n_row*n_col, particle_num, gray_feature_len);
+particle_alpha_hog = zeros(n_row*n_col, 1+template_num+hog_feature_len*2, particle_num);
+particle_alpha_gray = zeros(n_row*n_col, 1+template_num+gray_feature_len*2, particle_num);
+patch_particle_sim_hog = zeros(n_row*n_col, particle_num);
+patch_particle_sim_gray = zeros(n_row*n_col, particle_num);
+particle_sim_hog = zeros(1,particle_num);
+particle_sim_gray = zeros(1, particle_num);
+particle_sim = zeros(1, particle_num);
 particle_reind = zeros(1,particle_num);
 sigma_weight = 0.05;
 fig = figure(1);
@@ -121,26 +139,41 @@ for i=2:nframes
         object_candidate = imresize(object_candidate, [t_row t_col]);
         candidate_patches =  im_get_patches(object_candidate, n_row, n_col);
         for k=1:n_row*n_col
-            particle_features(k,j,:) = my_hog(candidate_patches(k,:,:),hog_bin_size, hog_n_orient, hog_index);
+            particle_features_hog(k,j,:) = my_hog(candidate_patches(k,:,:),hog_bin_size, hog_n_orient, hog_index);
+            particle_features_gray(k,j,:) = reshape(imresize(object_candidate, [gray_feature_h gray_feature_w]), gray_feature_len, 1);
         end
     end
     
     for j=1:n_row*n_col
-        patch_particle_feas = reshape(particle_features(j,:,:), particle_num, hog_feature_len);
+        patch_particle_feas_hog = reshape(particle_features_hog(j,:,:), particle_num, hog_feature_len);
         patch_HT = reshape(HT(j,:,:), 1+template_num+hog_feature_len*2, hog_feature_len);
-        particle_alpha(j,:,:) = my_lasso(patch_particle_feas', patch_HT', param);
-        patch_particle_sim(j,:) = reshape((sum(particle_alpha(j,1:1+template_num,:))+0.01)./(sum(particle_alpha(j,2+template_num:end,:))+0.01), 1, particle_num);
-        patch_particle_sim(j,:) = patch_particle_sim(j,:)/sum(patch_particle_sim(j,:));
+        particle_alpha_hog(j,:,:) = my_lasso(patch_particle_feas_hog', patch_HT', param);
+        patch_particle_sim_hog(j,:) = reshape((sum(particle_alpha_hog(j,1:1+template_num,:))+0.0001)./(sum(particle_alpha_hog(j,2+template_num:end,:))+0.0001), 1, particle_num);
+        patch_particle_sim_hog(j,:) = patch_particle_sim_hog(j,:)/sum(patch_particle_sim_hog(j,:));
+        
+        patch_particle_feas_gray = reshape(particle_features_gray(j,:,:), particle_num, gray_feature_len);
+        patch_GT = reshape(GT(j,:,:), 1+template_num+gray_feature_len*2, gray_feature_len);
+        particle_alpha_gray(j,:,:) = my_lasso(patch_particle_feas_gray', patch_GT', param);
+        patch_particle_sim_gray(j,:) = reshape((sum(particle_alpha_gray(j,1:1+template_num,:))+0.0001)./(sum(particle_alpha_gray(j,2+template_num:end,:))+0.0001), 1, particle_num);
+        patch_particle_sim_gray(j,:) = patch_particle_sim_gray(j,:)/sum(patch_particle_sim_gray(j,:));
         for k=1:particle_num
-           patch_particle_sim(j,k) = 1/sqrt(2*pi)/sigma_weight*exp(-(1-patch_particle_sim(j,k))/2/sigma_weight^2);
+           patch_particle_sim_hog(j,k) = 1/sqrt(2*pi)/sigma_weight*exp(-(1-patch_particle_sim_hog(j,k))/2/sigma_weight^2);
+           patch_particle_sim_gray(j,k) = 1/sqrt(2*pi)/sigma_weight*exp(-(1-patch_particle_sim_gray(j,k))/2/sigma_weight^2);
         end
-        patch_particle_sim(j,:) = patch_particle_sim(j,:)/sum(patch_particle_sim(j,:));
+        patch_particle_sim_hog(j,:) = patch_particle_sim_hog(j,:)/sum(patch_particle_sim_hog(j,:));
+        patch_particle_sim_gray(j,:) = patch_particle_sim_gray(j,:)/sum(patch_particle_sim_gray(j,:));
     end
-    big_sim_index = patch_particle_sim > sim_thresh;
-    particle_sim = sum(big_sim_index.*patch_particle_sim);
-    particle_sim = particle_sim / sum(particle_sim);
-    best_index = find(particle_sim == max(particle_sim));
-    particle_sim_matrix = repmat(particle_sim',1,4); 
+    big_sim_index_hog = patch_particle_sim_hog > sim_thresh_hog;
+    particle_sim_hog = sum(big_sim_index_hog.*patch_particle_sim_hog);
+    particle_sim_hog = particle_sim_hog / sum(particle_sim_hog);
+    %best_index = find(particle_sim_hog == max(particle_sim_hog));
+    
+    big_sim_index_gray = patch_particle_sim_gray > sim_thresh_gray;
+    particle_sim_gray = sum(big_sim_index_gray.*patch_particle_sim_gray);
+    particle_sim_gray = particle_sim_gray / sum(particle_sim_gray);
+    
+    particle_sim = hog_weight*particle_sim_hog+gray_weight*particle_sim_gray;
+    particle_sim_matrix = repmat(particle_sim',1,4);
     particles_temp = particles.*particle_sim_matrix;
     State = round(sum(particles_temp(:, :), 1));
     State
@@ -157,6 +190,7 @@ for i=2:nframes
     filename = sprintf('%d.jpg',i+start_frame-1);
     saveas(fig,filename,'jpg');
     
+
     %max(particle_sim)
     % Generate the new particles
     cdf_sim = cumsum(particle_sim);
@@ -184,6 +218,8 @@ for i=2:nframes
         template_patches = im_get_patches(template, n_row, n_col);
         for j=1:n_row*n_col
             HT(j,min_index+1,:) = my_hog(template_patches(j,:,:),hog_bin_size, hog_n_orient, hog_index);
+            object_patch_resize = imresize(reshape(template_patches(j,:,:), patch_h, patch_w), [gray_feature_h gray_feature_w]);
+            GT(j, min_index+1, :) = reshape(object_patch_resize, gray_feature_len, 1);
         end
         template_weight(min_index) = new_weight;
     end
